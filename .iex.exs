46a1b79ccc557889c6dcc67a3f3b7f20ad0ee350
Evan Purcer <ep@e2.gg>
2025-02-22 14:25:16 -0800

    add iex repl config and styling

---
+++ .iex.exs
@@ -0,0 +1,43 @@
+timestamp = fn ->
+  {_date, {hour, minute, _second}} = :calendar.local_time
+  [hour, minute]
+  |> Enum.map(&(String.pad_leading(Integer.to_string(&1), 2, "0")))
+  |> Enum.join(":")
+end
+
+IEx.configure(
+  colors: [
+    syntax_colors: [
+      number: :light_yellow,
+      atom: :light_cyan,
+      string: :light_black,
+      boolean: :red,
+      nil: [:magenta, :bright],
+    ],
+    ls_directory: :cyan,
+    ls_device: :yellow,
+  ],
+  default_prompt:
+    "#{IO.ANSI.green}%prefix#{IO.ANSI.reset} " <>
+    "::",
+  alive_prompt:
+    "#{IO.ANSI.green}%prefix#{IO.ANSI.reset} " <>
+    "(#{IO.ANSI.yellow}%node#{IO.ANSI.reset}) " <>
+    "::",
+  history_size: 10000,
+  inspect: [
+    pretty: true,
+    limit: :infinity,
+    width: 80
+  ],
+  width: 80,
+  auto_reload: true
+)
+
+fellowship = %{
+  hobbits: ["Frodo", "Samwise", "Merry", "Pippin"],
+  humans: ["Aragorn", "Boromir"],
+  dwarves: ["Gimli"],
+  elves: ["Legolas"],
+  wizards: ["Gandolf"]
+}
